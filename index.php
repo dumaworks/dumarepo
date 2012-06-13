1b14c291da57f02169a6e34e44dc106cb54629b8
Eric Kuto <ekuto@princeton.edu>
2012-06-13 15:34:22 -0400

    changed it back to today

--- index.php
+++ index.php
@@ -359,7 +359,7 @@ if (isset($_SESSION['myusername']))
 </tr>
 
 								<tr>
-									<td> <button class="btn large orange" type="submit">Join Saa hii!</button></td>
+									<td> <button class="btn large orange" type="submit">Join Today!</button></td>
 								</tr>
 
 
