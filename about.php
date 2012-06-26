783cb1e29869b2c63b3f113eb6526cb49da46a2c
Christine <cablauvelt@gmail.com>
2012-06-26 18:36:25 -0400

    about page

--- about.php
+++ about.php
@@ -1,35 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <?include "signup_header.php"?>
+    <?include "about_header.php"?>
     <div class="container">
 		<div class="content">
 			<div class="row" style="margin-left: 0px";>
 				<div class="span16">
 					<div class="row " style="margin-left: auto; margin-right: auto; margin-bottom: 0px">
 						<div class="span15" style="margin-top:25px;margin-bottom: 0px; margin-left: auto; margin-right: auto; ">
-						<h4 class="glow" style="font-family: 'Trebuchet MS', sans-serif; text-align: center; width: 100%; font-size: 40px; font-weight: lighter; color:#444444; "> &nbsp; About</h4>
+						<h4 class="glow" style="font-family: 'Lucida Grande', sans-serif; text-align: center; width: 100%; font-size: 40px; font-weight: lighter; color:#444444; "> &nbsp; About</h4>
 						</div>
 						
-						<div class="span15" style="margin-top:20px; margin-bottom: 0px; margin-left:auto; margin-right: auto;align:center;">
+						<div class="span15" style="margin-top:30px; margin-bottom: 0px; margin-left:auto; margin-right: auto;align:center;">
 <table style="background-color: #ffffff;opacity:0.8;align:center;margin-top:-10px;margin-left:45px;margin-bottom: 5px; width: 850px;">
 							<tbody>
 <tr>
-							<td style="padding:10px;font-family: 'Trebuchet MS', sans-serif; text-align: justify; font-weight: lighter; color:#444444; font-size: 15px; line-height: 20px;"> DUMA is two-pronged enterprise that leverages its SMS-based software and community theater outreaches to bring job networking, job text alerts, and job training to the informal sector of Kenya. </td>
-</tr>
-<tr>
-<td style="padding:10px;font-family: 'Trebuchet MS', sans-serif; text-align: justify; font-weight: lighter; color:#444444; font-size: 15px; line-height: 20px;"> Right now, the informal sector constitutes 75% of employed people and 98% of businesses in Kenya, but generates only 18% of GDP.  Moreover, job searching currently occurs via word-of-mouth, which restricts access to potential employees and encourages hiring with a disregard for skills.</td></tr>
-<tr>
-<td style="padding:10px;font-family: 'Trebuchet MS', sans-serif; text-align: justify; font-weight: lighter; color:#444444; font-size: 15px; line-height: 20px;"> DUMA is a game-changer that makes the hiring process easier, trustworthy, and more effective. Our software combines social networking and skills matching to ensure trust and reliability and to allow employees and employers to go beyond their existing contacts to find the perfect hire. We also facilitate temporary hiring to help businesses adapt when employees become sick, injured, or otherwise unavailable.  In Kenya, where 88% of the informal sector employs no more than two workers, just one absent employee can make a huge impact. Instead of shying away from a challenge, we capitalize on the unpredictable and irregular nature of informal sector jobs to produce new economic opportunities. </td></tr>
-<tr>
-<td style="padding:10px;font-family: 'Trebuchet MS', sans-serif; text-align: justify; font-weight: lighter; color:#444444; font-size: 15px; line-height: 20px;">DUMA personalizes the job search and sparks job creation as a high-speed, high-tech, job-networking platform for the developing world. 
+							<td style="padding:20px;font-family: 'Lucida Grande', sans-serif; text-align: justify; font-weight: lighter; color:#444444; font-size: 15.5px; line-height: 21px;"> <b> Imagine </b> that you are a trained mechanic in Kenya but you have given up searching for jobs in your industry. This is not because mechanic jobs do not exist in your town. It is because <i> TRUST </i> is one of the most important aspects of hiring in the developing world, and the people who would typically hire you - your family and friends - are not looking for mechanics. So instead, you are hawking wares on the street for <i> half the pay</i>.  After all, with a family to feed, you cannot afford to be choosy. <p></p><br> Now, <b> imagine </b> that you have access to an employment network where you can receive job alerts from friends of friends of friends who are hiring workers with your skills. </br><p></p><br> Now, what would you say if this is all happening on your cell phone? 
 							</td>
 </tr>
 </tbody>
 						</table>
 
 						</div>
-						<div class="span15" style="margin-bottom: 7px; margin-left: auto; margin-right: auto; text-align: center;">
+						<div class="span15" style="margin-top:30px; margin-bottom: 0px; margin-left:auto; margin-right: auto;align:center;">
+<table style="background-color: #ffffff;opacity:0.8;align:center;margin-top:-10px;margin-left:45px;margin-bottom: 5px; width: 850px;">
+							<tbody>
+<tr>
+							<td style="padding:20px;font-family: 'Lucida Grande', sans-serif; text-align: justify; font-weight: lighter; color:#444444; font-size: 15.5px; line-height: 21px;"> <b> DUMA </b> is a revolutionary job networking service for the developing world that uses basic cell phone technology and social networking to automatically connect employers to qualified employees whom they can trust. <p> </p><br> When an employer has a job opening, he or she sends a text message to DUMA to specify the available position. Our software then finds employees with the closest matching job skills and social contacts, and then sends them a text message notification about the job opening. So while the employee is now part of a robust hiring network, the employer also gains from a simple, more reliable, and more effective hiring process. <i> Everyone wins!</i>  </br>
+						</table>
+
+						</div>
+						<div class="span15" style="margin-top:30px; margin-bottom: 0px; margin-left:auto; margin-right: auto;align:center;">
+<table style="background-color: #ffffff;opacity:0.8;align:center;margin-top:-10px;margin-left:45px;margin-bottom: 5px; width: 850px;">
+							<tbody>
+<tr>
+							<td style="padding:20px;font-family: 'Lucida Grande', sans-serif; text-align: justify; font-weight: lighter; color:#444444; font-size: 15.5px; line-height: 21px;">  Today, in a world where cell phones are being used for everything from banking to health care, we’re excited about DUMA’s potential impact in Kenya and other emerging markets. By increasing the access of informal laborers to income generating activities, we hope to alleviate the direct impacts of poverty such as unsafe living conditions and food insecurity, while empowering members of the community with a sense of dignity and social significance.
+							</td>
+</tr>
+</tbody>
+						</table>
+
+						</div>
+						<div class="span15" style="margin-bottom: 7px; margin-left: auto; margin-right: auto;font-size: 15.5px text-align: center;">
 							<table width="100%" style="margin-bottom: 10px; margin-right: auto; margin-left: auto; text-align: center;">
 							<tbody>
 								<tr>
