f8edf0e56296211981cfb482cc3a3b2d52995787
Eric Kuto <ekuto@princeton.edu>
2012-06-28 13:57:24 -0400

    fixed email

--- contact.php
+++ contact.php
@@ -8,18 +8,16 @@
 				<div class="span16">
 					<div class="row " style="margin-left: auto; margin-right: auto; margin-bottom: 0px">
 						<div class="span15" style="margin-top:25px;margin-bottom: 0px; margin-left: auto; margin-right: auto; ">
-						<h4 class="glow" style="font-family: 'Trebuchet MS', sans-serif; text-align: center; width: 100%; font-size: 40px; font-weight: lighter; color:#444444; "> &nbsp; Contact Us</h4>
+						<h4 class="glow" style="font-family: 'Trebuchet MS', sans-serif; text-align: center; width: 100%; font-size: 40px; font-weight: lighter; color:#444444; "> &nbsp; Thank You!</h4>
 						</div>
 						
 						<div class="span15" style="margin-top:20px; margin-bottom: 0px; margin-left:auto; margin-right: auto;align:center;">
 <table style="opacity:0.8;align:center;margin-top:20px;margin-left:25px;margin-bottom: 5px; width: 850px;">
 							<tbody>
 <tr>
-							<td style="padding:5px;font-family: 'Trebuchet MS', sans-serif; text-align: center; font-weight: old; color:#444444; font-size: 25px; line-height: 20px;"> Email Address </td>
+							<td style="padding:5px;font-family: 'Trebuchet MS', sans-serif; text-align: center; font-weight: old; color:#444444; font-size: 25px; line-height: 20px;"> Your message has been sent and we shall get back to you in the next 24 hours.  </td>
 </tr>
-<tr>
-<td style="padding:5px;font-family: 'Trebuchet MS', sans-serif; text-align: center; font-weight: lighter; color:#444444; font-size: 20px; line-height: 20px;"> DumaWorks@gmail.com</td></tr>
-<tr></tbody>
+</tbody>
 						</table>
 
 						</div>
