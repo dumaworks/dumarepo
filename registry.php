c65dd1e5dc85a18cd9040c639b596abe885eaac6
Christine <cablauvelt@gmail.com>
2012-07-15 19:45:07 -0400

    message of the changes you made

--- registry.php
+++ registry.php
@@ -31,8 +31,8 @@
 										<p></p><p> <i> Note that contributions are not tax-deductible.</i></p> </td> </tr> 
 										</form>  																</br>
 
-										<tr><td style="padding-left:35px;padding-bottom:5px;padding-right:35px;width:900px;font-family: 'Tahoma', sans-serif; margin-left:15px;text-align: justify; color:#444444; font-size: 14px; line-height: 20px;"> We are so excited about DUMA’s potential and would love to make you a part of this experience. Your contribution gives DUMA a better shot at tackling the dire unemployment situation in Kenya, and every little bit counts.  </td> </tr>
- 										<tr><td style="padding-left:25px;padding-right:25px;padding-bottom:25px;"> <div class="fb-like" data-href="http://www.dumaworks.com/registry.php" data-send="true" data-width="450" data-show-faces="true"></div></td></tr>
+										<tr><td style="padding-left:35px;padding-bottom:15px;padding-right:35px;width:900px;font-family: 'Tahoma', sans-serif; margin-left:15px;text-align: justify; color:#444444; font-size: 14px; line-height: 20px;"> We are so excited about DUMA’s potential and would love to make you a part of this experience. Your contribution gives DUMA a better shot at tackling the dire unemployment situation in Kenya, and every little bit counts.  </td> </tr>
+ 										<tr><td style="padding-left:35px;padding-right:25px;padding-bottom:25px;"> <div class="fb-like" data-href="http://www.dumaworks.com/registry.php" data-send="true" data-width="450" data-show-faces="true"></div></td></tr>
 
  								</tbody>
 							</table>
